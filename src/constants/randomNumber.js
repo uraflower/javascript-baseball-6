aa264f5e2638baccc8953a42eabce6f161d8ca1c
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-25 16:18:26 +0900

    feat(난수): 3자리 고유 랜덤 숫자 생성 함수 추가

---
+++ src/constants/randomNumber.js
@@ -0,0 +1,7 @@
+const DIGIT = 3;
+const RANGE = Object.freeze({
+  min: 1,
+  max: 9,
+});
+
+export { DIGIT, RANGE };
---
+++ src/generateUniqueRandomNumber.js
@@ -0,0 +1,17 @@
+import { Random } from '@woowacourse/mission-utils';
+import { DIGIT, RANGE } from './constants/randomNumber.js';
+
+function generateUniqueRandomNumber() {
+  const numbers = [];
+
+  while (numbers.length < DIGIT) {
+    const number = Random.pickNumberInRange(RANGE.min, RANGE.max);
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  }
+
+  return numbers;
+}
+
+export default generateUniqueRandomNumber;
