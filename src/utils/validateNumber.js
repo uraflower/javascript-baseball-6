b7279b496de68b0a87235d0088162d18037a9797
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-25 19:55:23 +0900

    feat(예외): 입력 값 길이가 3이 아닌 경우 예외 처리

--- src/utils/validateNumber.js
+++ src/utils/validateNumber.js
@@ -1,9 +1,15 @@
+import { DIGIT } from '../constants/randomNumber.js';
+
+function isInvalidLength(str) {
+  return str.length !== DIGIT;
+}
+
 function isDuplicated(str) {
   return new Set([...str]).size < str.length;
 }
 
 function isInvalidNumber(number) {
-  return isDuplicated(number);
+  return isDuplicated(number) || isInvalidLength(number);
 }
 
 export default isInvalidNumber;
