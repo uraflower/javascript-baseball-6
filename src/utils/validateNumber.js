2a514d4edc6c54a0fe8ccc37cad42591f0cd9896
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-25 20:28:52 +0900

    feat(예외): 입력 값 범위 예외 처리

--- src/utils/validateNumber.js
+++ src/utils/validateNumber.js
@@ -1,4 +1,4 @@
-import { DIGIT } from '../constants/randomNumber.js';
+import { DIGIT, RANGE } from '../constants/randomNumber.js';
 
 function isInvalidLength(str) {
   return str.length !== DIGIT;
@@ -8,8 +8,17 @@ function isDuplicated(str) {
   return new Set([...str]).size < str.length;
 }
 
+function isNotInRange(str) {
+  const result = [...str].find(
+    (num) => Number(num) < RANGE.min || Number(num) > RANGE.max,
+  );
+  return result !== undefined;
+}
+
 function isInvalidNumber(number) {
-  return isDuplicated(number) || isInvalidLength(number);
+  return (
+    isDuplicated(number) || isInvalidLength(number) || isNotInRange(number)
+  );
 }
 
 export default isInvalidNumber;
