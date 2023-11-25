37d9e6d67e9b716a31d3281601b62ca4d43471dd
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-25 18:35:54 +0900

    feat(컨트롤러): 반복하여 정답 맞히는 기능 추가

--- src/Controller/GameController.js
+++ src/Controller/GameController.js
@@ -1,4 +1,6 @@
 import User from '../Domain/User.js';
+import getHint from '../Domain/getHint.js';
+import { DIGIT } from '../constants/randomNumber.js';
 import generateUniqueRandomNumber from '../utils/generateUniqueRandomNumber.js';
 
 class GameController {
@@ -9,7 +11,23 @@ class GameController {
   async play() {
     this.#computer = generateUniqueRandomNumber();
 
-    this.#user = User.getNumber();
+    await this.#repeatGuess();
+    // 게임 종료 메시지 출력
+    // ...
+  }
+
+  async #repeatGuess() {
+    let isDone = false;
+    while (!isDone) {
+      isDone = await this.#guess();
+    }
+  }
+
+  async #guess() {
+    this.#user = await User.getNumber();
+    const [strike, ball] = getHint(this.#computer, this.#user);
+    // 결과 출력
+    return strike === DIGIT;
   }
 }
 
