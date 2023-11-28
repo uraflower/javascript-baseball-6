143a30b07eb0328db7e270746713283ef6fd7b5c
uraflower <82873315+uraflower@users.noreply.github.com>
2023-11-29 00:03:26 +0900

    refactor(에러): 매직 넘버 제거

--- src/constants/messages/error.js
+++ src/constants/messages/error.js
@@ -1,8 +1,8 @@
-import { DIGIT } from '../randomNumber.js';
+import { DIGIT, RANGE } from '../randomNumber.js';
 
 const ERROR = Object.freeze({
   prefix: '[ERROR]',
-  invalidNumber: `중복되지 않는 1~9 사이 ${DIGIT}자리 숫자를 입력해주세요.`,
+  invalidNumber: `중복되지 않는 ${RANGE.min}~${RANGE.max} 사이 ${DIGIT}자리 숫자를 입력해주세요.`,
   invalidRestartFlag: '올바른 값을 입력해주세요.',
 });
 
